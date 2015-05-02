5de038aeb4bdc02afb8db132d8aec8b6a7b91ca3
majikojima <ykk61890@yahoo.co.jp>
2015-05-02 17:03:39 +0900

    #18 コンストラクタを使ってみよう

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,33 @@
-//メソッド
+//クラス
 
 /*
+クラス：設計図
+-メンバー変数（フィールド）
+-メソッド
 
+インスタンス：実体化
 */
+
+class User{
+	String name;
+	String email;
+
+	//コンストラクタ
+	User(String name, String email){
+		this.name = name;
+		this.email = email;
+	}
+
+	void sayHi(){
+		System.out.println("Hi " + this.name);
+	}
+}
+
 public class HelloWorld{
 	public static void main(String[] args){
-		sayHi("ooo");
-		sayHi();
-	}
-
-	public static void sayHi(String name){
-		System.out.println("Hi " + name);
-	}
-
-	//メソッドのオーバーロード
-	public static void sayHi(){
-		System.out.println("Hi nobody");
+		User tom = new User("tom","tomtom");
+		User bob = new User("bob","bobbob");
+		tom.sayHi();
+		bob.sayHi();
 	}
 }
