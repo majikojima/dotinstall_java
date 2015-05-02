95cc787a711cdb9e0172add9b0cf2457c5a67397
majikojima <ykk61890@yahoo.co.jp>
2015-05-02 16:00:59 +0900

    #04 Javaで扱えるデータ型について

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 //変数とデータ型
 
+/*
+boolean true/false
+char 一文字 'a'
+byte / short / int / long
+float / double
+
+String 文字列 ""
+*/
 public class HelloWorld{
 	public static void main(String[] args){
 		int x;
