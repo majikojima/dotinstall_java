417173ff38112737e91c899628dbe4292dc81b3a
majikojima <ykk61890@yahoo.co.jp>
2015-05-02 16:45:29 +0900

    #16 メソッドを使ってみよう

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,20 @@
-//配列
+//メソッド
 
 /*
 
 */
 public class HelloWorld{
 	public static void main(String[] args){
-		int sales[];
-		sales = new int[4];
-		sales[0] = 150;
-		sales[1] = 200;
-		sales[2] = 140;
-		sales[3] = 400;
-		
-		for(int i=0; i < sales.length; i++){
-			System.out.println(sales[i]);
-		}
+		sayHi("ooo");
+		sayHi();
+	}
+
+	public static void sayHi(String name){
+		System.out.println("Hi " + name);
+	}
+
+	//メソッドのオーバーロード
+	public static void sayHi(){
+		System.out.println("Hi nobody");
 	}
 }
