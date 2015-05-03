ead6e442bcbe340096efa516035f82347d977c9c
majikojima <ykk61890@yahoo.co.jp>
2015-05-03 10:09:26 +0900

    防御的コピー

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,50 @@
-//Stringクラス
+//http://popp915.blog58.fc2.com/blog-entry-299.html
+//防御的コピー
+
+import java.util.Date;
 
 public class HelloWorld{
 	public static void main(String[] args){
-		String s = new String("abcdefg");
+		Date badDate = new Date();
+		BadPeriod bad = new BadPeriod(badDate);
+		//内部の値を変更する
+		badDate.setYear(1000);
 
-		System.out.println(s.length());
-		System.out.println(s.substring(2,5));
-		System.out.println(s.replaceAll("abc", "ABC"));
-		System.out.println(s);
-		
-		String s1 = "Sun,Mon,Tue";
-		String[] youbi = s1.split("," , 0);
-		for(int i=0; i < youbi.length; i++){
-			System.out.println(youbi[i]);
-		}
+		Date goodDate = new Date();
+		GoodPeriod good = new GoodPeriod(goodDate);
+		//内部の値を変更する
+		goodDate.setYear(1000);
 
+		System.out.println(bad.getDate());
+		System.out.println(good.getDate());
+	}
+}
+
+//防御的コピーが出来ていないクラス
+class BadPeriod{
+	private final Date date;
+
+	//コンストラクタ
+	public BadPeriod(Date date){
+		this.date = date;
+	}
+
+	public Date getDate(){
+		return date;
+	}
+}
+
+//防御的コピーが出来ているクラス
+class GoodPeriod{
+	private final Date date;
+
+	//コンストラクタ
+	public GoodPeriod(Date date){
+		//引数として渡ってきた値を取り出し、新たにオブジェクトを生成しましょう。
+		this.date = new Date(date.getTime());
+	}
+
+	public Date getDate(){
+		return new Date(this.date.getTime());
 	}
 }
