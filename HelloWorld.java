8fdfb62ce7ff8537ec7da7b9769a3cc42958c73b
majikojima <ykk61890@yahoo.co.jp>
2015-05-05 13:58:28 +0900

    Set

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,50 +1,24 @@
-//http://popp915.blog58.fc2.com/blog-entry-299.html
-//防御的コピー
-
-import java.util.Date;
+//http://www.javaroad.jp/java_collection3.htm
+//List・・・重複要素を許可。要素の順番を保持
+//Setインタフェースは重複要素を持たないオブジェクトの集合を実装します
+import java.util.*;
 
 public class HelloWorld{
+	void showFeature(Set exSet, String s){
+		int[] intArray = {5,8,2,9,1};
+		for(int i=0; i<intArray.length; i++){
+			exSet.add(intArray[i]);
+		}
+		System.out.println(s + "=" + exSet);
+	}
+
 	public static void main(String[] args){
-		Date badDate = new Date();
-		BadPeriod bad = new BadPeriod(badDate);
-		//内部の値を変更する
-		badDate.setYear(1000);
-
-		Date goodDate = new Date();
-		GoodPeriod good = new GoodPeriod(goodDate);
-		//内部の値を変更する
-		goodDate.setYear(1000);
-
-		System.out.println(bad.getDate());
-		System.out.println(good.getDate());
-	}
-}
-
-//防御的コピーが出来ていないクラス
-class BadPeriod{
-	private final Date date;
-
-	//コンストラクタ
-	public BadPeriod(Date date){
-		this.date = date;
-	}
-
-	public Date getDate(){
-		return date;
-	}
-}
-
-//防御的コピーが出来ているクラス
-class GoodPeriod{
-	private final Date date;
-
-	//コンストラクタ
-	public GoodPeriod(Date date){
-		//引数として渡ってきた値を取り出し、新たにオブジェクトを生成しましょう。
-		this.date = new Date(date.getTime());
-	}
-
-	public Date getDate(){
-		return new Date(this.date.getTime());
+		HelloWorld object = new HelloWorld();
+		//順序はランダム
+		object.showFeature(new HashSet(), "HashSet");
+		//順序は昇順
+		object.showFeature(new TreeSet(), "TreeSet");
+		//順序は挿入順
+		object.showFeature(new LinkedHashSet(), "LinkedHashSet");
 	}
 }
